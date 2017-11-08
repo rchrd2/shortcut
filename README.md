2a4c7f037e4adb2beae95ac8085d324e4740a3a7
Richard Caceres <richard@archive.org>
2017-11-07 23:33:15 -0800

    shortcut v1.0.0

--- README.md
+++ README.md
@@ -1,11 +1,48 @@
 # shortcut
 
-* Coming Soon *
+Create command line shortcuts.
 
-Create command line shortcuts
 
 ## Install
 
 ```
 npm install -g shortcut-cli
 ```
+
+
+## Usage
+
+```
+$ shortcut add example "cd /path/to/example"
+$ `shortcut example`
+$ shortcut add example "cd /path/to/updated/example"
+$ shortcut remove example
+$ shortcut add diskspace "du -hs"
+$ `shortcut diskspace`
+156k
+$ shortcut list
+diskspace: du -hs
+example: cd /path/to/updated/example
+```
+
+
+## Notes
+
+As far as I can tell, it's not possible for a child process to change the working directory (eg `cd`) of the parent process. Since `cd` is one of my primary use cases, I opted to have the program print the command, which can then be executed with backticks, eg:
+
+```
+`shortcut cd-somewhere`
+```
+
+I am still considering having a way to run commands, or possibly running all commands except `cd` commands.
+
+
+## License
+
+MIT
+
+
+
+## Credits
+
+Richard Caceres - [@rchrd2](https://github.com/rchrd2)
--- bin/shortcut.js
+++ bin/shortcut.js
@@ -1,2 +1,152 @@
 #! /usr/bin/env node
-console.log('Coming Soon!');
+
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Config file is a JSON file with an array of objects
+ */
+var configFilePath = path.join(process.env.HOME, '.shortcut.json');
+
+
+/**
+ * @param string name - the name
+ * @param string value - the shell command to run
+ */
+function updateShortcut(name, value) {
+  var shortcuts = readShortcuts();
+
+  var shortcutIndex = null;
+  for (var i = 0; i < shortcuts.length; i++) {
+    if (shortcuts[i].name === name) {
+      shortcutIndex = i;
+      break;
+    }
+  }
+
+  if (value === null && shortcutIndex !== null) {
+    // Remove the shortcut
+    shortcuts.splice(shortcutIndex, 1);
+  } else if (shortcutIndex !== null) {
+    // Update the shortcut
+    shortcuts[shortcutIndex].value = value;
+  } else {
+    // Add the shortcut
+    shortcuts.push({'name': name, 'value': value});
+  }
+
+  // Write to the config file
+  var fileContents = JSON.stringify(shortcuts, null, 2);
+  fs.writeFileSync(configFilePath, fileContents, 'utf8');
+}
+
+/**
+ * @return array
+ * @note might exit(1) on error
+ */
+function readShortcuts() {
+  if (!fs.existsSync(configFilePath)) {
+    return [];
+  } else {
+    var fileContents = fs.readFileSync(configFilePath, 'utf8');
+    var shortcuts;
+    try {
+      shortcuts = JSON.parse(fileContents);
+    } catch (e) {
+      console.log('Error parsing config file. Please review the file and retry: ' + configFilePath);
+      process.exit(1);
+    }
+    return shortcuts;
+  }
+}
+
+/**
+ * @param string name
+ * @return object|undefined
+ */
+function readShortcut(name, shortcuts) {
+  if (!shortcuts) shortcuts = readShortcuts();
+  return shortcuts.filter(function(row) {
+    return row.name == name;
+  }).pop();
+}
+
+/**
+ * Runs the shortcut in the cli
+ * @param string name
+ */
+function runShortcut(name) {
+  var shortcut = readShortcut(name);
+  if (!shortcut) {
+    console.log(name + ' does not exist');
+    process.exit(1);
+  } else {
+    // TODO run shortcut.value;
+    console.log(shortcut.value);
+    process.exit(0);
+  }
+}
+
+/**
+ * Lists all the shortcuts
+ */
+function listShortcuts() {
+  var shortcuts = readShortcuts()
+  shortcuts.sort(function(a,b) {
+    return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+  });
+  shortcuts.forEach(function(row) {
+    console.log(row.name + ': ' + row.value);
+  });
+}
+
+function runTest() {
+  console.log('Creating "test" command"');
+  updateShortcut('test', 'echo hi');
+  listShortcuts();
+  runShortcut('test');
+}
+
+function showHelp() {
+  console.log(`Available commands:
+add,remove,list,help,_test
+
+Example usage:
+$ shortcut add example "cd /path/to/example"
+$ \`shortcut example\`
+$ shortcut add example "cd /path/to/updated/example"
+$ shortcut remove example
+
+Current shortcuts:`);
+  listShortcuts();
+}
+
+function main() {
+  var action = process.argv[2];
+  var args = process.argv.slice(3);
+  switch (action) {
+    case 'add':
+      updateShortcut(args[0], args.slice(1).join(' '));
+      break;
+    case 'remove':
+      updateShortcut(args[0], null);
+      break;
+    case 'list':
+      listShortcuts();
+      break;
+    case '_test':
+      runTest();
+      break;
+    case 'help':
+    case '':
+    case undefined:
+      showHelp();
+      break;
+    default:
+      runShortcut(action);
+      break;
+  }
+  process.exit(0);
+}
+
+main();
